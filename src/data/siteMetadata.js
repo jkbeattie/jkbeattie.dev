74d6657374d8fdb8ab4ae5362e24267e3b9768f0
Jayden Beattie <jaydenk.beattie@gmail.com>
2022-03-07 21:43:50 +1100

    initial commit

---
+++ src/data/siteMetadata.js
@@ -0,0 +1,11 @@
+const siteMetadata = {
+    title: 'Jayden Beattie | Full Stack Software-Engineer',
+    author: 'Jayden Beattie',
+    language: 'en-us',
+    siteUrl: 'https://jkbeattie.dev',
+    siteRepo: 'https://github.com/jkbeattie/jkbeattie.dev',
+    email: 'hello@jkbeattie.dev',
+    
+}
+
+module.exports = siteMetadata;
