8cc27671a398a6362b6b146ede0315bd62bf5342
Jayden Beattie <jaydenk.beattie@gmail.com>
2022-03-23 21:19:42 +1100

    feature: incorporate tailwind

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,8 @@
 function Hero() {
     return (
-        <div className="flex">
-            
+        <div className="hero">
+            <h1 className="text-blue-400">Hi, my name is Jayden.</h1>
+            <h1>I'm a Full Stack Software Engineer</h1>
         </div>
     )
 }
