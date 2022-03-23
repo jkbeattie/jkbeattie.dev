a3f191a325a5347c80a8581309ce49bf02ba7d06
Jayden Beattie <jaydenk.beattie@gmail.com>
2022-03-23 21:50:34 +1100

    feature: begins the hero section layout and styling

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,8 @@
 function Hero() {
     return (
-        <div className="hero">
-            <h1 className="text-blue-400">Hi, my name is Jayden.</h1>
-            <h1>I'm a Full Stack Software Engineer</h1>
+        <div className="hero min-w-full min-h-full flex flex-col justify-items-center">
+            <h1 className="text-5xl font-bold">Hi, my name is <span className="text-sky-600">Jayden</span></h1>
+            <h1 className="text-5xl font-bold">I'm a Full Stack Software Engineer.</h1>
         </div>
     )
 }
