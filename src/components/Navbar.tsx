e5bef1d1b0fef3cf95ec952fb100e9e8354e2887
Jayden Beattie <jaydenk.beattie@gmail.com>
2022-03-30 22:54:34 +1100

    feature: creates NavItem component, checks isActive status of page via react-router, styling of active links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,12 @@
-import { Link } from 'react-router-dom'
+import NavItem from './NavItem'
 
 function Navbar() {
     return (
         <div className='navbar mt-5 flex flew-row place-content-center'>
             <div className='links font-main font-light text-base'>
-                <Link to='/' className='nav-link font-bold'>Home</Link>
-                <Link to='/about' className='nav-link'>About</Link>
-                <Link to='/contact' className='nav-link'>Contact</Link>
+                <NavItem href='/' title='Home' />
+                <NavItem href='/about' title='About' />
+                <NavItem href='/contact' title='Contact' />
             </div>
         </div>
     )
