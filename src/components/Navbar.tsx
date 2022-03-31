1616e32803e3c6210d33ebb574dee132248ab8f8
Jayden Beattie <jaydenk.beattie@gmail.com>
2022-03-31 16:43:03 +1100

    feature: adds avatar to the Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,12 @@
 import NavItem from './NavItem'
+import Avatar from '../static/avatar.png'
 
 function Navbar() {
     return (
-        <div className='navbar mt-5 flex flew-row place-content-center'>
+        <div className='navbar mt-5 flex flew-row justify-between items-center'>
+            <div className='avatar'>
+                <img src={Avatar} alt="jkbeattie.dev" />
+            </div>
             <div className='links font-main font-light text-base'>
                 <NavItem href='/' title='Home' />
                 <NavItem href='/about' title='About' />
