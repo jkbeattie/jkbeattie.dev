9a3a9d9451ea415fc90bd90a257f89326dcd152e
Jayden Beattie <Jayden.Beattie@austcommunitymedia.com.au>
2022-04-04 13:42:12 +1000

    initial commit

---
+++ src/components/TechStack.tsx
@@ -0,0 +1,9 @@
+function TechStack() {
+    return (
+        <div className="tech-stack flex flex-col items-center">
+            <h1 className="text-3xl font-bold font-heading">My Technology Stack</h1>
+        </div>
+    )
+}
+
+export default TechStack
