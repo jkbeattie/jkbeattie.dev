cdd082b468652349992f50cd49a8dc91884d80f9
Jayden Beattie <Jayden.Beattie@austcommunitymedia.com.au>
2022-04-04 14:50:01 +1000

    feature: begins scaffolding the technology stack table

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,7 +1,11 @@
+import TechStackTable from "./TechStackTable"
+
 function TechStack() {
     return (
         <div className="tech-stack flex flex-col items-center">
             <h1 className="text-3xl font-bold font-heading">My Technology Stack</h1>
+            <p className="text-lg font-light font-main">As a software engineer, I believe flexibility and adaptability is key -- therefore, I use a range of tools and technologies to build software solutions.</p>
+            <TechStackTable />
         </div>
     )
 }
---
+++ src/components/TechStackTable.tsx
@@ -0,0 +1,17 @@
+function TechStackTable() {
+    return (
+        <div className="tech-stack-table grid grid-cols-3">
+            <div className="frontend tech-stack-column">
+                Frontend
+            </div>
+            <div className="tech-stack-column backend">
+                Backend
+            </div>
+            <div className="tech-stack-column devops">
+                DevOps
+            </div>
+        </div>
+    )
+}
+
+export default TechStackTable
