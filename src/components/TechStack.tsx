6c1b2b0bbf4b2d682c3ebbfa46ccf305c4e81b90
Jayden Beattie <jaydenk.beattie@gmail.com>
2022-04-04 20:57:16 +1000

    feature: markup and styling of the technology stack component

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -3,8 +3,9 @@ import TechStackTable from "./TechStackTable"
 function TechStack() {
     return (
         <div className="tech-stack flex flex-col items-center">
-            <h1 className="text-3xl font-bold font-heading">My Technology Stack</h1>
-            <p className="text-lg font-light font-main">As a software engineer, I believe flexibility and adaptability is key -- therefore, I use a range of tools and technologies to build software solutions.</p>
+            <h1 className="text-3xl font-bold font-heading leading-loose">My Technology Stack</h1>
+            <p className="text-lg font-thin font-main leading-loose">I write clean and organised code, with a passion for helping my clients achieve their goals.</p>
+            <p className="text-lg font-thin font-main leading-loose">When it comes to delivering high-quality software solutions, I use a range of tools and technologies to meet my clients needs.</p>
             <TechStackTable />
         </div>
     )
--- src/components/TechStackTable.tsx
+++ src/components/TechStackTable.tsx
@@ -1,14 +1,49 @@
+import { FaLaptop, FaCode, FaCodeBranch } from 'react-icons/fa'
+
 function TechStackTable() {
     return (
         <div className="tech-stack-table grid grid-cols-3">
-            <div className="frontend tech-stack-column">
-                Frontend
+            <div className="tech-stack-column frontend">
+                <FaLaptop className='tech-stack-icon' />
+                <p className="font-heading font-bold text-lg mt-5">Front-End</p>
+                <p className="font-main font-thin text-lg text-center mx-2 my-3">I prioritse designing and developing clean user interfaces that follow thoughtful design patterns to help my clients engage their audience.</p>
+                <p className="font-main font-extralight text-lg text-center text-blue-600">Proficient in:</p>
+                <ul className="skills font-main font-thin text-lg text-center">
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript / TypeScript</li>
+                    <li>React.js</li>
+                    <li>Sass</li>
+                    <li>Tailwind CSS</li>
+                </ul>
             </div>
             <div className="tech-stack-column backend">
-                Backend
+                <FaCode className='tech-stack-icon' />
+                <p className='font-heading font-bold text-lg mt-5'>Back-End</p>
+                <p className="font-main font-thin text-lg text-center mx-2 my-3">I place a large emphasis on developing secure, robust, scalable and maintainable back-end infrastructure for your website or application.</p>
+                <p className="font-main font-extralight text-lg text-center text-blue-600">Skilled in:</p>
+                <ul className="skills font-main font-thin text-lg text-center">
+                    <li>Python</li>
+                    <li>.NET Core (C#)</li>
+                    <li>Flask / Django</li>
+                    <li>Node.js</li>
+                    <li>PostgreSQL / MySQL / MongoDB</li>
+                    <li>REST APIs</li>
+                </ul>
             </div>
             <div className="tech-stack-column devops">
-                DevOps
+                <FaCodeBranch className='tech-stack-icon' />
+                <p className='font-heading font-bold text-lg mt-5'>DevOps</p>
+                <p className="font-main font-thin text-lg text-center mx-2 my-3">I'm experienced using a range of tools to manage my applications which allows me to continually deliver high-quality software solutions.</p>
+                <p className="font-main font-extralight text-lg text-center text-blue-600">Experienced in:</p>
+                <ul className="skills font-main font-thin text-lg text-center">
+                    <li>Git</li>
+                    <li>Docker</li>
+                    <li>Jenkins</li>
+                    <li>Kubernetes</li>
+                    <li>Terraform</li>
+                    <li>AWS</li>
+                </ul>
             </div>
         </div>
     )
