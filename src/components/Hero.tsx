df6fc4bc796f07c6f435c8404bc04b57322058a3
Jayden Beattie <Jayden.Beattie@austcommunitymedia.com.au>
2022-04-05 11:13:02 +1000

    feature: finalising the technology stack markup and styling

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ import { Button } from './Button';
 
 function Hero() {
     return (
-        <div className="hero flex flex-col place-content-center">
+        <div className="hero flex flex-col place-content-center wrapper">
             <h1 className="text-5xl font-bold font-heading leading-tight">Hi, my name is <span className="text-blue-600">Jayden Beattie</span></h1>
             <h1 className="text-5xl font-bold font-heading leading-tight">
                 <Typical
--- src/components/TechStackTable.tsx
+++ src/components/TechStackTable.tsx
@@ -5,10 +5,10 @@ function TechStackTable() {
         <div className="tech-stack-table grid grid-cols-3">
             <div className="tech-stack-column frontend">
                 <FaLaptop className='tech-stack-icon' />
-                <p className="font-heading font-bold text-lg mt-5">Front-End</p>
-                <p className="font-main font-thin text-lg text-center mx-2 my-3">I prioritse designing and developing clean user interfaces that follow thoughtful design patterns to help my clients engage their audience.</p>
-                <p className="font-main font-extralight text-lg text-center text-blue-600">Proficient in:</p>
-                <ul className="skills font-main font-thin text-lg text-center">
+                <p className="font-heading font-bold text-xl mt-5">Front-End</p>
+                <p className="font-main font-light text-lg text-center mx-3 my-3">I prioritse designing and developing clean user interfaces that follow thoughtful design patterns to help my clients engage their audience.</p>
+                <p className="font-main font-normal text-lg text-center text-blue-600 mt-8">Proficient in:</p>
+                <ul className="skills font-main font-light text-lg text-center">
                     <li>HTML</li>
                     <li>CSS</li>
                     <li>JavaScript / TypeScript</li>
@@ -19,10 +19,10 @@ function TechStackTable() {
             </div>
             <div className="tech-stack-column bordered backend">
                 <FaCode className='tech-stack-icon' />
-                <p className='font-heading font-bold text-lg mt-5'>Back-End</p>
-                <p className="font-main font-thin text-lg text-center mx-2 my-3">I place a large emphasis on developing secure, robust, scalable and maintainable back-end infrastructure for your website or application.</p>
-                <p className="font-main font-extralight text-lg text-center text-blue-600">Skilled in:</p>
-                <ul className="skills font-main font-thin text-lg text-center">
+                <p className='font-heading font-bold text-xl mt-5'>Back-End</p>
+                <p className="font-main font-light text-lg text-center mx-3 my-3">I place a large emphasis on developing secure, robust, scalable and maintainable back-end infrastructure for your website or application.</p>
+                <p className="font-main font-normal text-lg text-center text-blue-600 mt-8">Skilled in:</p>
+                <ul className="skills font-main font-light text-lg text-center">
                     <li>Python</li>
                     <li>.NET Core (C#)</li>
                     <li>Flask / Django</li>
@@ -33,10 +33,10 @@ function TechStackTable() {
             </div>
             <div className="tech-stack-column devops">
                 <FaCodeBranch className='tech-stack-icon' />
-                <p className='font-heading font-bold text-lg mt-5'>DevOps</p>
-                <p className="font-main font-thin text-lg text-center mx-2 my-3">I'm experienced using a range of tools to manage my applications which allows me to continually deliver high-quality software solutions.</p>
-                <p className="font-main font-extralight text-lg text-center text-blue-600">Experienced in:</p>
-                <ul className="skills font-main font-thin text-lg text-center">
+                <p className='font-heading font-bold text-xl mt-5'>DevOps</p>
+                <p className="font-main font-light text-lg text-center mx-3 my-3">I'm experienced using a range of tools to manage my applications which allows me to continually deliver high-quality software solutions.</p>
+                <p className="font-main font-normal text-lg text-center text-blue-600 mt-8">Experienced in:</p>
+                <ul className="skills font-main font-light text-lg text-center">
                     <li>Git</li>
                     <li>Docker</li>
                     <li>Jenkins</li>
