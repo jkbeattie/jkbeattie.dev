d4394abf0068315d6b2f0ca946a3ea97a9a632a5
Jayden Beattie <Jayden.Beattie@austcommunitymedia.com.au>
2022-04-05 15:23:03 +1000

    feature: begins basic footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,15 @@
+import { FiLinkedin, FiCodepen, FiGithub } from 'react-icons/fi'
+
 function Footer() {
     return (
-        
+        <section className="footer wrapper flex flex-row justify-between py-20">
+            <div className="copyright text-gray-600">© 2022 Jayden Beattie</div>
+            <div className="social-icons flex flex-row">
+                <FiLinkedin className="social-icon" />
+                <FiCodepen className='social-icon' />
+                <FiGithub className='social-icon' />
+            </div>
+        </section>
     )
 }
 
