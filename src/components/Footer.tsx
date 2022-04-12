4b180e6c0acc253bbcbe7996ce6a576ea8a98631
Jayden Beattie <Jayden.Beattie@austcommunitymedia.com.au>
2022-04-12 12:11:10 +1000

    fix: typo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
         <section className="footer wrapper flex flex-row justify-between py-20">
             <div className="copyright text-gray-600">© 2022 Jayden Beattie</div>
             <div className="social-icons flex flex-row">
-                <a href='https://www.linkedin.com/in/jkbeattie/'><FiLinkedin className="social-icon" /></a>
+                <a href='https://www.linkedin.com/in/jkbeattie'><FiLinkedin className="social-icon" /></a>
                 <a href='https://codepen.io/jkbeattie'><FiCodepen className='social-icon' /></a>
                 <a href='https://github.com/jkbeattie'><FiGithub className='social-icon' /></a>
             </div>
