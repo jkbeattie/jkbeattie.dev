fe303f40ac7b98b6561c9712a14d7d4a2de0f2a2
Jayden Beattie <jaydenk.beattie@gmail.com>
2022-05-30 20:47:26 +1000

    chore: restart the skills and technologies pane

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,15 @@
 import Footer from '../components/Footer';
 import Hero from '../components/Hero';
 import Navbar from '../components/Navbar';
-import Technology from '../components/Technology';
 
 function Home() {
     return (
         <div>
             <Navbar />
             <Hero />
-            <Technology />
             <Footer />
         </div>
     )
 }
 
-export default Home;    
+export default Home;
